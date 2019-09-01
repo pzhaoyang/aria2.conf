f182af5c480c3e0296b3002a6aab9d907035cfc0
Ubuntu <pzhaoyang>
2019-09-01 00:06:23 +0000

    auto install h5ai server

--- install.sh
+++ install.sh
@@ -1,8 +1,44 @@
-#!/bin/sh
+#!/bin/sh -x
 
-workdir=/home/www/aria2
-sudo mkdir -p  $workdir
-sudo cp -rvf aria2.* $workdir
-sudo cp aria2.service /lib/systemd/system
+webroot=/home/www
+confdir=$webroot/aria2.conf.d
+h5aidir=$webroot/_h5ai
+nginxconf=/etc/nginx/sites-enabled/default
+serviceconf=/lib/systemd/system/aria2.service
+
+
+echo "install dependence package"
+sudo -E apt install nginx aria2
+
+echo "Stop already started service"
+sudo systemctl stop aria2
+sudo systemctl disable aria2
+sudo systemctl stop nginx
+sudo systemctl disable nginx
+sudo umount $webroot/data
+
+echo "Setup Environment"
+sudo rm -rf $webroot
+sudo mkdir -p  $confdir
+sudo mkdir -p  $webroot/data
+
+sudo rsync -az _h5ai $webroot 
+sudo rsync -az aria2 $h5aidir
+sudo rsync -az aria2.* $confdir
+
+sudo rm -rf $serviceconf
+sudo ln -sf $confdir/aria2.service $serviceconf
+
+sudo ln -sf $confdir/aria2.nginx.h5ai.conf $nginxconf
+
+echo "Start services"
+sudo mount -t exfat /dev/sda1 $webroot/data
+
 sudo systemctl start aria2
+sudo systemctl start nginx
+
 sudo systemctl enable aria2
+sudo systemctl enable nginx
+
+
+
