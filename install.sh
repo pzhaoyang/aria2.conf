bae436f52d0834fbd5f112e5f7934cf91e7736a2
wrangler <pzhaoyang@126.com>
2019-09-10 15:34:32 +0000

    add samba conf

--- install.sh
+++ install.sh
@@ -4,17 +4,21 @@ webroot=/home/www
 confdir=$webroot/aria2.conf.d
 h5aidir=$webroot/_h5ai
 nginxconf=/etc/nginx/sites-enabled/default
+smbconf=/etc/samb/smb.conf
 serviceconf=/lib/systemd/system/aria2.service
 
 
 echo "install dependence package"
-sudo -E apt install nginx aria2 php7.2-fpm
+sudo -E apt install nginx aria2 php7.2-fpm samba samba-common
 
 echo "Stop already started service"
 sudo systemctl stop aria2
 sudo systemctl disable aria2
 sudo systemctl stop nginx
 sudo systemctl disable nginx
+sudo systemctl stop smbd
+sudo systemctl disable smbd
+sudo umount $webroot/data
 sudo umount $webroot/data
 
 echo "Setup Environment"
@@ -25,6 +29,7 @@ sudo mkdir -p  $webroot/data
 sudo rsync -az _h5ai $webroot 
 sudo rsync -az aria2 $h5aidir
 sudo rsync -az aria2.* $confdir
+sudo rsync -az smb.conf $smbconf
 
 sudo rm -rf $serviceconf
 sudo ln -sf $confdir/aria2.service $serviceconf
@@ -34,11 +39,13 @@ sudo ln -sf $confdir/aria2.nginx.h5ai.conf $nginxconf
 echo "Start services"
 sudo mount -t exfat /dev/sda1 $webroot/data
 
-sudo systemctl start aria2
-sudo systemctl start nginx
+sudo systemctl restart aria2
+sudo systemctl restart nginx
+sudo systemctl restart smbd
 
 sudo systemctl enable aria2
 sudo systemctl enable nginx
+sudo systemctl enable smbd
 
 
 
